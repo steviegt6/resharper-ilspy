8579ea281813f9ef9bd48bbf286894c561e0d1f4
Stephen T <xxlennygamerxx@gmail.com>
2024-07-30 03:27:38 -0500

    Create base project.

---
+++ src/Tomat.ReSharperPatcher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using CliFx;
+
+namespace Tomat.ReSharperPatcher;
+
+internal static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        Environment.GetFolderPath(Environment.SpecialFolder)
+        return await new CliApplicationBuilder()
+                    .SetTitle("r# patcher")
+                    .SetVersion('v' + typeof(Program).Assembly.GetName().Version?.ToString())
+                    .SetDescription("a tool to patch r#")
+                    .AddCommandsFromThisAssembly()
+                    .Build()
+                    .RunAsync(args);
+    }
+}
