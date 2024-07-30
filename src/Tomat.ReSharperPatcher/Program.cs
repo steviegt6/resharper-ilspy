f94da10f4d6f086532231526a2cc421aeb2e9b28
Stephen T <xxlennygamerxx@gmail.com>
2024-07-30 04:22:56 -0500

    Delete useless platform stuff.

--- src/Tomat.ReSharperPatcher/Program.cs
+++ src/Tomat.ReSharperPatcher/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 
-using Tomat.ReSharperPatcher.Platform;
-
 // TODO: Eventually make this actually good.
 // Platform-agnostic, abstractions for patches, smart handling for caching, etc.
 
@@ -10,51 +8,14 @@ namespace Tomat.ReSharperPatcher;
 
 internal static class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         Console.WriteLine("rsp (r# patcher)");
         Console.WriteLine("a tool to patch r#");
         Console.WriteLine('v' + typeof(Program).Assembly.GetName().Version?.ToString());
 
-        var platform = InitializePlatform();
-
         var rsHostDir = ResolveReSharperHostDir() ?? throw new DirectoryNotFoundException("Could not resolve ReSharperHost directory");
         Console.WriteLine("ReSharperHost directory: " + rsHostDir);
-    }
-
-    private static IPlatform InitializePlatform()
-    {
-        IPlatform platform;
-
-        if (OperatingSystem.IsWindows())
-        {
-            platform = new WindowsPlatform();
-        }
-        else if (OperatingSystem.IsMacOS())
-        {
-            throw new PlatformNotSupportedException("macOS isn't supported; PR a platform implementation");
-        }
-        else if (OperatingSystem.IsLinux())
-        {
-            throw new PlatformNotSupportedException("Linux isn't supported; PR a platform implementation");
-        }
-        else
-        {
-            throw new PlatformNotSupportedException();
-        }
-
-        try
-        {
-            Directory.CreateDirectory(platform.GetRspDataDirectory());
-        }
-        catch (Exception e)
-        {
-            throw new InvalidOperationException($"Cannot initialize platform, failed to verify data directory '{platform.GetRspDataDirectory()}'", e);
-        }
-
-        Console.WriteLine("Using platform implementation: " + platform.GetType().Name);
-        Console.WriteLine("Using data directory: "          + platform.GetRspDataDirectory());
-        return platform;
     }
 
     private static string? ResolveReSharperHostDir()
